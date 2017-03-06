c919f8f68d09c2ee47f76d5c7429a5b4e6a495fd
weibin268 <binweizhuang@hotmail.com>
2017-03-07 00:02:37 +0800

    aa

--- src/main/java/com/zhuang/log/App.java
+++ src/main/java/com/zhuang/log/App.java
@@ -1,13 +1,21 @@
 package com.zhuang.log;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Logger logger = LoggerFactory.getLogger(App.class);
+    	
+    	logger.debug("aaaaa");
+    	logger.debug("bbbbb");
+    	
     }
 }
